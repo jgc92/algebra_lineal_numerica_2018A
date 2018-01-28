410addbe01fe988881c33d0bdd2766b6fa891771
JGC <joaquin@yandex.com>
2018-01-28 11:09:47 -0600

    finish ex3

--- Assignment_1/ex3.py
+++ Assignment_1/ex3.py
@@ -5,11 +5,8 @@ new_eps = eps
 
 while eps != new_eps + 1.0:
     new_eps = new_eps/2
-    print(new_eps)
 
-
-print("eps = " + str(eps))
-print("new_eps = " + str(new_eps))
+print("Machine Epsilon = " + str(new_eps))
 
 
     
